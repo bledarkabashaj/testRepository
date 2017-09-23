85f295675eadc7e80561a25640f3401933fc6dfd
fluffybled <bledarkabashaj@gmail.com>
2017-09-23 23:11:15 +0200

    Messing around now

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Redundant Title</title>
+    </head>
+
+    <body>
+        <?php
+            echo "messing around with github";
+        ?>
+
+    </body>
+
+</html>
